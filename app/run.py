a8923695688045ad9910a7538367ced31b433891
Vagner Zeizer <vzeizer@gmail.com>
2021-04-11 16:04:55 -0300

    third submission

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/bank.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('InsertTableName', engine)
 
 # load model
-model = joblib.load("../models/zeizer_model.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
